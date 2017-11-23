010741f4037792844346b76c86b9232ceb7b8cad
wpsaputra <wayanpermana93@gmail.com>
2017-11-23 16:50:31 +0800

    Add Lihat Fenomena

--- views/fenomena/index.php
+++ views/fenomena/index.php
@@ -2,6 +2,10 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use app\models\SfSumber;
+use app\models\SfKecamatan;
+use app\models\SfLapanganUsaha;
+use app\models\SfPengaruh;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\FenomenaSearch */
@@ -24,14 +28,47 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            // 'id',
             'tanggal_fenomena',
-            'sumber_id',
-            'kecamatan_id',
+            // 'sumber_id',
+            [
+                'attribute' => 'sumber_id',
+                'value' => function($model){
+                    return SfSumber::findOne($model->sumber_id)->nama;
+                    
+                }
+            ],
+
+            // 'kecamatan_id',
+            [
+                'attribute' => 'kecamatan_id',
+                'value' => function($model){
+                    return SfKecamatan::findOne($model->kecamatan_id)->nama;
+                    
+                }
+            ],
+
+
             'isi_fenomena:ntext',
+            
             // 'lapangan_usaha',
+            [
+                'attribute' => 'lapangan_usaha',
+                'value' => function($model){
+                    return SfLapanganUsaha::findOne($model->lapangan_usaha)->keterangan;
+                    
+                }
+            ],
+
             // 'pengaruh_id',
-            // 'tanggal_entri',
+            [
+                'attribute' => 'pengaruh_id',
+                'value' => function($model){
+                    return SfPengaruh::findOne($model->pengaruh_id)->nama;
+                }
+            ],
+
+            'tanggal_entri',
             // 'upload_foto_dokumen:ntext',
             // 'isVerified',
 
