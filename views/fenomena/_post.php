da29275a4271a47afcb39da6ddcbb162ed42627b
wpsaputra <wayanps93@gmail.com>
2017-11-24 15:39:57 +0800

    add download button

--- views/fenomena/_post.php
+++ views/fenomena/_post.php
@@ -49,6 +49,16 @@ use yii\helpers\HtmlPurifier;
                         <br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
 
+                    <div class="dropdown pull-right">
+                        <button class="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown">Download
+                        <span class="caret"></span></button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">HTML</a></li>
+                            <li><a href="#">CSS</a></li>
+                            <li><a href="#">JavaScript</a></li>
+                        </ul>
+                    </div>
+
 
                 </div>
             </div>
