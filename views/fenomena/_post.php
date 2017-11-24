593b9f612aeed1f41095da549e5c62e855b21086
wayanpermana <aco.wayan@outlook.com>
2017-11-24 19:39:49 +0800

    To do add dropdown from db

--- views/fenomena/_post.php
+++ views/fenomena/_post.php
@@ -22,42 +22,85 @@ for($i=0; $i<count($array_image_dokumen); $i++){
         <div class="col-md-6">
             <div class="panel panel-default">
                 <div class="panel-body">
+                    
                     <!-- Carousel -->
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+                    <div id="myCarousel<?php echo $model->id; ?>" class="carousel slide" data-ride="carousel" data-interval="false">
                         <!-- Indicators -->
                         <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <!-- <li data-target="#myCarousel<?php //echo $model->id; ?>" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel<?php //echo $model->id; ?>" data-slide-to="1"></li>
+                            <li data-target="#myCarousel<?php //echo $model->id; ?>" data-slide-to="2"></li> -->
+
+                            <?php
+                                for($i=0; $i<count($array_image); $i++){
+                                    $carousel = '<li data-target="#myCarousel?id" data-slide-to="?i" class="?class"></li>';
+                                    if($i==0){
+                                        $carousel = str_replace("?id", $model->id, $carousel);
+                                        $carousel = str_replace("?i", $i, $carousel);
+                                        $carousel = str_replace("?class", "active", $carousel);
+                                    }else{
+                                        $carousel = str_replace("?id", $model->id, $carousel);
+                                        $carousel = str_replace("?i", $i, $carousel);
+                                        $carousel = str_replace("?class", "", $carousel);
+                                    }
+                                    echo $carousel;
+    
+                                }
+            
+                            ?>
+
                         </ol>
 
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner">
-                            <div class="item active">
-                            <img src="http://localhost/sifeni/web/uploads/1.jpg" alt="Los Angeles">
+                            <!-- <div class="item active">
+                            <img src="http://localhost:1000/sifeni/web/uploads/1.jpg" alt="Los Angeles">
                             </div>
 
                             <div class="item">
-                            <img src="http://localhost/sifeni/web/uploads/2.jpg" alt="Chicago">
+                            <img src="http://localhost:1000/sifeni/web/uploads/2.jpg" alt="Chicago">
                             </div>
 
                             <div class="item">
-                            <img src="http://localhost/sifeni/web/uploads/3.jpg" alt="New York">
-                            </div>
+                            <img src="http://localhost:1000/sifeni/web/uploads/3.jpg" alt="New York">
+                            </div> -->
+
+                            <?php
+                                for($i=0; $i<count($array_image); $i++){
+                                    $carousel_inner = '
+                                        <div class="?class">
+                                        ?src
+                                        </div>
+                                    ';
+                                    if($i==0){
+                                        $carousel_inner = str_replace("?class", 'item active', $carousel_inner);
+                                        $carousel_inner = str_replace("?src", Html::img('@web/uploads/'.$array_image[$i]), $carousel_inner);
+                                    }else{
+                                        $carousel_inner = str_replace("?class", 'item', $carousel_inner);
+                                        $carousel_inner = str_replace("?src", Html::img('@web/uploads/'.$array_image[$i]), $carousel_inner);
+                                    }
+                                    echo $carousel_inner;
+    
+                                }
+            
+                            ?>
+
+
+
                         </div>
 
                         <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <a class="left carousel-control" href="#myCarousel<?php echo $model->id; ?>" data-slide="prev">
                             <span class="glyphicon glyphicon-chevron-left"></span>
                             <span class="sr-only">Previous</span>
                         </a>
-                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <a class="right carousel-control" href="#myCarousel<?php echo $model->id; ?>" data-slide="next">
                             <span class="glyphicon glyphicon-chevron-right"></span>
                             <span class="sr-only">Next</span>
                         </a>
                     </div>
                     <p>
-                        <br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <br/> <?php echo $model->isi_fenomena; ?>
                     </p>
 
                     <div class="dropdown pull-right">
