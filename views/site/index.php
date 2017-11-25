d894f75ee6503b7edbdb08fb663bdcffe6886f79
wpsaputra <wayanps93@gmail.com>
2017-11-25 13:00:58 +0800

    Add home menu

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,111 @@
 <?php
 
+use yii\helpers\Url;
+
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+        <img src="http://localhost/sifeni/web/uploads/1.jpg" alt="Los Angeles">
+        </div>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <div class="item">
+        <img src="http://localhost/sifeni/web/uploads/2.jpg" alt="Chicago">
+        </div>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <div class="item">
+        <img src="http://localhost/sifeni/web/uploads/3.jpg" alt="New York">
+        </div>
     </div>
 
-    <div class="body-content">
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    </div>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+    <div class="container">
+         <!-- Marketing Icons Section -->
+         <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Selamat Datang di SiFENI
+                </h1>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Lihat Fenomena</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Menu ini berguna untuk melihat fenomena yang sudah diinput di dalam aplikasi SiFeni. Klik tombol lanjut di bawah untuk masuk ke dalam menu.</p>
+                        <a href="<?php echo Url::to(['fenomena/index']);?>" class="btn btn-default">Lanjut</a>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-gift"></i> Entri Fenomena</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Menu ini berguna untuk mengentri fenomena di dalam aplikasi SiFeni. Klik tombol lanjut di bawah untuk masuk ke dalam menu.</p>
+                        <a href="<?php echo Url::to(['fenomena/create']);?>" class="btn btn-default">Lanjut</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-compass"></i> Foto &amp; Dokumen</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Menu ini berguna untuk melihat foto &amp; dokumen fenomena di dalam aplikasi SiFeni. Klik tombol lanjut di bawah untuk masuk ke dalam menu.</p>
+                        <a href="<?php echo Url::to(['fenomena/list']);?>" class="btn btn-default">Lanjut</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+        <!-- Portfolio Section -->
+        
+        <!-- /.row -->
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>Untuk kritik & saran terkait pengembangan SiFENI bisa menghubungi saudara <a href="mailto:fito@bps.go.id">Suharjufito Endo, SST</a><br/> (fito@bps.go.id)</p>
+                </div>
+                <div class="col-md-4"></div>
+                    <!-- <a class="btn btn-lg btn-default btn-block" href="mailto:fito@bps.go.id">Call to Action</a> -->
+                </div>
             </div>
         </div>
 
+        <hr>
     </div>
+
+
 </div>
